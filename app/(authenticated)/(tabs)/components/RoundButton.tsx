b6524ac22ec6f89b242c4be1f3ac1248f52f4ec8
Furkan Yılmaz <furkanyilmaz@Furkan-Mac-mini.home>
2024-04-25 21:29:40 +0300

    Skeleton of home page is prepared. RoundButton component is made

---
+++ app/(authenticated)/(tabs)/components/RoundButton.tsx
@@ -0,0 +1,40 @@
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import React from "react";
+import { Ionicons } from "@expo/vector-icons";
+import Colors from "@/constants/Colors";
+
+type RountButtonProps = {
+  name: string;
+  image: typeof Ionicons.defaultProps;
+  onPress: () => void;
+};
+
+const RoundButton = ({ name, image, onPress }: RountButtonProps) => {
+  return (
+    <TouchableOpacity style={styles.container} onPress={onPress}>
+      <View style={styles.circle}>
+        <Ionicons name={image} size={30} color={Colors.dark} />
+      </View>
+      <Text style={styles.label}>{name}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: { alignItems: "center", gap: 10 },
+  circle: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    backgroundColor: Colors.lightGray,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  label: {
+    fontSize: 16,
+    fontWeight: "500",
+    color: Colors.dark,
+  },
+});
+
+export default RoundButton;
--- app/(authenticated)/(tabs)/home.tsx
+++ app/(authenticated)/(tabs)/home.tsx
@@ -1,12 +1,50 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView, StyleSheet, Button } from "react-native";
 import React from "react";
+import Colors from "@/constants/Colors";
+import RoundButton from "./components/RoundButton";
 
 const home = () => {
+  const balance = 1453;
+
+  const onAddMoney = () => {};
+
   return (
-    <View>
-      <Text>home</Text>
-    </View>
+    <ScrollView style={{ backgroundColor: Colors.background }}>
+      <View style={styles.account}>
+        <View style={styles.row}>
+          <Text style={styles.balance}>{balance}</Text>
+          <Text style={styles.currency}>$</Text>
+        </View>
+      </View>
+
+      <View style={styles.actionRow}>
+        <RoundButton name="Add Money" image={"add"} onPress={onAddMoney} />
+        <RoundButton name="Exchange" image={"refresh"} onPress={onAddMoney} />
+        <RoundButton name="List" image={"list"} onPress={onAddMoney} />
+        <RoundButton name="Add Money" image={"add"} onPress={onAddMoney} />
+      </View>
+    </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  account: {
+    margin: 80,
+    alignItems: "center",
+  },
+  row: {
+    flexDirection: "row",
+    alignItems: "flex-end",
+    justifyContent: "center",
+    gap: 10,
+  },
+  balance: { fontSize: 50, fontWeight: "bold" },
+  currency: { fontSize: 20, fontWeight: "500" },
+  actionRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: 20,
+  },
+});
 
 export default home;
